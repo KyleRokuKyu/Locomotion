9be7ba436629d6326f22ab8c676c9f6e7c439e5e
RokuKyu <kyle@killbotstudio.com>
2021-11-14 16:09:05 -0500

    Added "IC" esque Occlusion and Physics Culling, Self Sizing, Colorizable Box

---
+++ The Actual Mod/Locomotion/src/client/IntegratedCircuit.cs
@@ -0,0 +1,233 @@
+﻿using LogicAPI.Data;
+using LogicWorld;
+using LogicWorld.Rendering.Components;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using LogicAPI;
+using LogicWorld.Rendering.Chunks;
+using LogicWorld.ClientCode;
+using JimmysUnityUtilities;
+
+namespace Locomotion
+{
+	class IntegratedCircuit : ComponentClientCode<IntegratedCircuit.IData>, IColorableClientCode
+	{
+		public interface IData
+		{
+			Color24 Color { get; set; }
+		}
+
+		protected struct PhysicalComponent
+		{
+			public List<Collider> colliders;
+			public ComponentAddress addy;
+			public List<Decoration> decorations;
+			public List<RenderedEntity> entities;
+			public List<Mesh> meshes;
+		}
+
+		List<PhysicalComponent> physicalComponents;
+
+		Color24 IColorableClientCode.Color
+		{
+			get
+			{
+				return Data.Color;
+			}
+			set
+			{
+				Data.Color = value;
+			}
+		}
+
+		public string ColorsFileKey => "Interactables";
+
+		public float MinColorValue => 0f;
+
+		protected override void SetDataDefaultValues()
+		{
+			Data.Color = Color24.Purple;
+		}
+
+		protected override void DataUpdate()
+		{
+			SetBlockColor(Data.Color);
+		}
+
+		protected override void InitializeInGhost()
+		{
+			try
+			{
+				if (Component != null && Component.Parent != null)
+				{
+					if (!WorldRenderer.Entities.GetClientCode(Component.Parent).ToString().Contains("CircuitBoard"))
+					{
+						return;
+					}
+					physicalComponents = new List<PhysicalComponent>();
+
+					FindAndSetScale();
+
+					SetActive(false);
+				}
+			}
+			catch (Exception e)
+			{
+				Debug.Log("IntegratedCircuit.InitializeInWorld: It broke. Idk mang. " + e);
+			}
+		}
+
+		protected override void InitializeInWorld()
+		{
+			try
+			{
+				if (Component != null && Component.Parent != null)
+				{
+					if (!WorldRenderer.Entities.GetClientCode(Component.Parent).ToString().Contains("CircuitBoard"))
+					{
+						return;
+					}
+					physicalComponents = new List<PhysicalComponent>();
+
+					FindAndSetScale();
+
+					SetActive(false);
+				}
+			}
+			catch (Exception e)
+			{
+				Debug.Log("IntegratedCircuit.InitializeInWorld: It broke. Idk mang. " + e);
+			}
+		}
+
+		void FindAndSetScale()
+		{
+			try
+			{
+				ComponentAddress pAddress = Component.Parent;
+				RecursivelyFindComponents(pAddress);
+				Bounds bounds = new Bounds(Component.WorldPosition, Vector3.zero);
+				foreach (PhysicalComponent component in physicalComponents)
+				{
+					foreach (Collider c in component.colliders)
+					{
+						if (c != WorldRenderer.EntityColliders.GetCollidersOfComponent(Component.Parent)[0])
+							bounds.Encapsulate(c.bounds);
+					}
+				}
+
+				GetBlockEntity(0).WorldPosition = bounds.center - new Vector3(SceneAndNetworkManager.MainWorld.Data.Lookup(pAddress).up.x * bounds.size.x, SceneAndNetworkManager.MainWorld.Data.Lookup(pAddress).up.y * bounds.size.y, SceneAndNetworkManager.MainWorld.Data.Lookup(pAddress).up.z * bounds.size.z) / 2f;
+				GetBlockEntity(0).Scale = bounds.size - new Vector3(0.01f, 0.01f, 0.01f);
+
+			}
+			catch (Exception e)
+			{
+				Debug.Log("IntegratedCircuit.FindAndSetScale: It broke. Idk mang. " + e);
+			}
+		}
+
+		void RecursivelyFindComponents(ComponentAddress address)
+		{
+			try
+			{
+				PhysicalComponent tempComponent = new PhysicalComponent()
+				{
+					colliders = new List<Collider>(),
+					addy = address,
+					decorations = new List<Decoration>(),
+					entities = new List<RenderedEntity>(),
+					meshes = new List<Mesh>()
+				};
+				foreach (Collider c in WorldRenderer.EntityColliders.GetCollidersOfComponent(address))
+				{
+					tempComponent.colliders.Add(c);
+				}
+				foreach (Decoration decoration in WorldRenderer.Entities.GetDecorations(address))
+				{
+					tempComponent.decorations.Add(decoration);
+				}
+				foreach (RenderedEntity entity in WorldRenderer.Entities.GetBlockEntitiesAt(address))
+				{
+					tempComponent.entities.Add(entity);
+					tempComponent.meshes.Add(entity.Mesh);
+				}
+				foreach (WireAddress wire in SceneAndNetworkManager.MainWorld.Data.GetWiresDirectlyAttachedTo(address)) {
+					tempComponent.entities.Add((RenderedEntity)WorldRenderer.Entities.GetWireEntity(wire));
+					tempComponent.meshes.Add(WorldRenderer.Entities.GetWireEntity(wire).Mesh);
+				}
+				physicalComponents.Add(tempComponent);
+				foreach (ComponentAddress item in SceneAndNetworkManager.MainWorld.Data.Lookup(address).EnumerateChildren())
+				{
+					RecursivelyFindComponents(item);
+				}
+			}
+			catch (Exception e)
+			{
+				Debug.Log("IntegratedCircuit.RecursivelyFindColliders: It broke. Idk mang. " + e);
+			}
+		}
+
+
+		protected override void OnComponentDestroyed()
+		{
+			try
+			{
+				if (Component != null && Component.Parent != null)
+					SetActive(true);
+			}
+			catch (Exception e)
+			{
+				Debug.Log("IntegratedCircuit.OnComponentDestroyed: It broke. Idk mang. " + e);
+			}
+		}
+
+		void SetActive(bool state)
+		{
+			try
+			{
+				if (physicalComponents == null)
+					return;
+				foreach (PhysicalComponent c in physicalComponents)
+				{
+					if (c.addy == Component.Parent)
+						continue;
+					if (c.colliders[0] == WorldRenderer.EntityColliders.GetCollidersOfComponent(Address)[0])
+					{
+						continue;
+					}
+					if (WorldRenderer.Entities.GetClientCode(c.addy) != null && WorldRenderer.Entities.GetClientCode(c.addy).ToString().Contains("Socket"))
+					{
+						continue;
+					}
+					foreach (Decoration d in c.decorations)
+					{
+						d.DecorationObject.SetActive(state);
+					}
+					foreach (Collider col in c.colliders)
+					{
+						col.enabled = state;
+					}
+					if (state)
+					{
+						for (int i = 0; i < c.entities.Count; i++)
+						{
+							c.entities[i].SetMesh(c.meshes[i]);
+						}
+					}
+					else
+					{
+						foreach (RenderedEntity entity in c.entities)
+						{
+							entity.SetMesh(new Mesh());
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Debug.Log("IntegratedCircuit.SetActive: It broke. Idk mang. " + e);
+			}
+		}
+	}
+}
--- The Actual Mod/Locomotion/src/client/Main.cs
+++ The Actual Mod/Locomotion/src/client/Main.cs
@@ -1,5 +1,10 @@
 ﻿using GameDataAccess;
+using LogicAPI;
 using LogicAPI.Client;
+using LogicAPI.Data;
+using LogicWorld;
+using LogicWorld.Interfaces;
+using System;
 using System.IO;
 using UnityEngine;
 
@@ -9,22 +14,22 @@ namespace Locomotion
     {
         public static CustomSoundEffect PistonUp { get; private set; }
         public static CustomSoundEffect PistonDown { get; private set; }
-		public static CustomSoundClipDatabase clipDatabase { get; private set; }
+		public static CustomSoundClipDatabase ClipDatabase { get; private set; }
 
 		public static void TryLoadSoundEffects ()
 		{
 			if (PistonUp != null)
 				return;
 
-			clipDatabase = new CustomSoundClipDatabase();
+			ClipDatabase = new CustomSoundClipDatabase();
 
 			PistonUp = new CustomSoundEffect();
 			PistonDown = new CustomSoundEffect();
 
 			CustomSoundEffectData data = new CustomSoundEffectData() { VolumeScale = 10f, ClipRelativePaths = new string[1] { "Locomotion/sounds/clips/PistonUp.wav" } };
-			PistonUp.Initialize(data, "Interaction", clipDatabase);
+			PistonUp.Initialize(data, "Interaction", ClipDatabase);
 			data = new CustomSoundEffectData() { VolumeScale = 10f, ClipRelativePaths = new string[1] { "Locomotion/sounds/clips/PistonDown.wav" } };
-			PistonDown.Initialize(data, "Interaction", clipDatabase);
+			PistonDown.Initialize(data, "Interaction", ClipDatabase);
 		}
 
         public static Texture2D LoadImage(string filename)
@@ -49,5 +54,136 @@ namespace Locomotion
                 return texture;
             }
         }
-    }
+
+		public static void StopRenderingExceptThis(ComponentAddress cAddress, IWorldRenderer worldRenderer)
+		{
+			ComponentAddress pAddress = SceneAndNetworkManager.MainWorld.Data.Lookup(cAddress).Parent;
+
+			foreach (ComponentAddress item in SceneAndNetworkManager.MainWorld.Data.Lookup(pAddress).EnumerateChildren())
+			{
+				if (item == cAddress)
+					continue;
+				try
+				{
+					if (worldRenderer.Entities.GetClientCode(item) != null && worldRenderer.Entities.GetClientCode(item).ToString().Contains("Socket"))
+					{
+						continue;
+					}
+				}
+				catch (Exception e)
+				{
+					Debug.Log("StopRenderingExceptThis: It broke. Idk mang. " + e);
+				}
+				worldRenderer.EntityManager.StopRenderingComponentRecursive(item);
+			}
+			worldRenderer.EntityManager.StopRenderingComponent(pAddress);
+			foreach (WireAddress item2 in SceneAndNetworkManager.MainWorld.Data.GetWiresDirectlyAttachedTo(pAddress))
+			{
+				worldRenderer.EntityManager.StopRenderingWire(item2);
+			}
+		}
+
+		public static void StartRendering(ComponentAddress cAddress, IWorldRenderer worldRenderer)
+		{
+			try
+			{
+				if (cAddress == null)
+					return;
+				//worldRenderer.EntityManager.RenderComponent(cAddress);
+				StartRenderingRecursively(cAddress, worldRenderer);
+			}
+			catch (Exception e)
+			{
+				Debug.Log("StartRendering: It broke. Idk mang. " + e);
+			}
+		}
+
+		public static void StartRenderingRecursively(ComponentAddress cAddress, IWorldRenderer worldRenderer)
+		{
+			Debug.Log("1");
+			try
+			{
+				if (cAddress != null)
+				{
+					Debug.Log("2");
+					worldRenderer.EntityManager.RenderComponent(cAddress);
+					//worldRenderer.EntityManager.ReRenderComponentsAndWiresRecursive(cAddress);
+				}
+				Debug.Log("3");
+				foreach (ComponentAddress item in SceneAndNetworkManager.MainWorld.Data.Lookup(cAddress).EnumerateChildren())
+				{
+					Debug.Log("4");
+					if (item == cAddress)
+						continue;
+					Debug.Log("5");
+					StartRenderingRecursively(item, worldRenderer);
+					Debug.Log("6");
+					/*
+					foreach (WireAddress item2 in SceneAndNetworkManager.MainWorld.Data.GetWiresDirectlyAttachedTo(item))
+					{
+						Debug.Log("7");
+						worldRenderer.EntityManager.RenderWire(item2);
+					}
+					*/
+					Debug.Log("8");
+				}
+				Debug.Log("9");
+			}
+			catch (Exception e)
+			{
+				Debug.Log("StartRenderingRecursively: It broke. Idk mang. " + e);
+			}
+		}
+
+		public static void StartRenderingExceptThis(ComponentAddress cAddress, ComponentAddress oAddress, IWorldRenderer worldRenderer)
+		{
+			try
+			{
+				if (oAddress == null)
+					return;
+				StartRenderingRecursively(oAddress, worldRenderer);
+			}
+			catch (Exception e)
+			{
+				Debug.Log("StartRendering: It broke. Idk mang. " + e);
+			}
+		}
+
+		public static void StartRenderingRecursivelyExceptThis(ComponentAddress cAddress, ComponentAddress oAddress, IWorldRenderer worldRenderer)
+		{
+			Debug.Log("1");
+			try
+			{
+				if (oAddress != null)
+				{
+					Debug.Log("2");
+					worldRenderer.EntityManager.RenderComponent(oAddress);
+					//worldRenderer.EntityManager.ReRenderComponentsAndWiresRecursive(cAddress);
+				}
+				Debug.Log("3");
+				foreach (ComponentAddress item in SceneAndNetworkManager.MainWorld.Data.Lookup(oAddress).EnumerateChildren())
+				{
+					Debug.Log("4");
+					if (item == cAddress || item == oAddress)
+						continue;
+					Debug.Log("5");
+					StartRenderingRecursively(item, worldRenderer);
+					Debug.Log("6");
+					/*
+					foreach (WireAddress item2 in SceneAndNetworkManager.MainWorld.Data.GetWiresDirectlyAttachedTo(item))
+					{
+						Debug.Log("7");
+						worldRenderer.EntityManager.RenderWire(item2);
+					}
+					*/
+					Debug.Log("8");
+				}
+				Debug.Log("9");
+			}
+			catch (Exception e)
+			{
+				Debug.Log("StartRenderingRecursively: It broke. Idk mang. " + e);
+			}
+		}
+	}
 }
